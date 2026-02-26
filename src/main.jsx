044234821c83f42904b459b046ed6da2ae565120
Yaaza Ait Bah <aitbah.dev@gmail.com>
2026-02-26 09:12:30 +0000

    feat(i18n): charger la configuration i18next au demarrage de l'application

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import { Provider } from "react-redux";
 import { store } from "./store";
+import "./i18n";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
