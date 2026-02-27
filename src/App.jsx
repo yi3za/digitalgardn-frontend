cb30f7ff32a27efbd571836fd25b443f08018915
Yaaza Ait Bah <aitbah.dev@gmail.com>
2026-02-27 08:31:30 +0000

    refactor(app): deleger le rendu des pages au composant AppRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,11 @@
+import AppRouter from "./routes/AppRouter";
+
+/**
+ * Composant principal de l'application
+ *
+ * Charge et retourne le routeur principal (AppRouter)
+ * qui gere la navigation entre les differentes pages
+ */
 export default function App() {
-  return <>App</>;
+  return <AppRouter />;
 }
