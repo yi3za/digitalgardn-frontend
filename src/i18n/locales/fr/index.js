2bf840abb34c5a5e73089f5cd617f264f786edf4
Yaaza Ait Bah <aitbah.dev@gmail.com>
2026-03-02 14:49:43 +0000

    refactor(i18n): modulariser les fichiers de traduction francais

--- src/i18n/locales/fr/index.js
+++ src/i18n/locales/fr/index.js
@@ -1,8 +1,12 @@
+import codes from "./codes.json";
+import auth from "./auth.json";
+
 /**
  * Ce fichier regroupe toutes les traductions pour la langue française
  */
 export const fr = {
   translation: {
-    //
+    ...codes,
+    ...auth,
   },
 };
