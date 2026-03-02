6b0e19ed31320d2759d23f516e7cd0dcc3b4913f
Yaaza Ait Bah <aitbah.dev@gmail.com>
2026-03-02 12:19:00 +0000

    refactor(i18n): supprimer les fichiers de traduction obsoletes

--- src/i18n/locales/fr/index.js
+++ src/i18n/locales/fr/index.js
@@ -1,10 +1,8 @@
-import { codes } from "./codes";
-
 /**
  * Ce fichier regroupe toutes les traductions pour la langue française
  */
 export const fr = {
   translation: {
-    ...codes,
+    //
   },
 };
