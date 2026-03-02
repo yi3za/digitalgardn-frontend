f530a449cc5797d4827c4c47dab5b453dbefd2c5
Yaaza Ait Bah <aitbah.dev@gmail.com>
2026-03-02 14:52:42 +0000

    refactor(ui): simplifier les imports des composants UI avec un fichier d'index

---
+++ src/components/ui/index.js
@@ -0,0 +1,13 @@
+export { Button } from "./button";
+export {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "./card";
+export { Form, FormControl, FormField, FormItem, FormMessage } from "./form";
+export { Input } from "./input";
+export { Label } from "./label";
