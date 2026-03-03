09bf89b173511bd8437ff3b6789c1294954a7096
Yaaza Ait Bah <aitbah.dev@gmail.com>
2026-03-03 15:05:20 +0000

    feat(app): rendre le systeme de notifications global

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,13 @@ import { Provider } from "react-redux";
 import { store } from "./store";
 import "@/i18n";
 import "@/styles/index.css";
+import { Toaster } from "@/components/ui/sonner";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
       <App />
+      <Toaster position="top-center" />
     </Provider>
   </StrictMode>,
 );
