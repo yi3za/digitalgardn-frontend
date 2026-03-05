e18956c18bb9bd7bceedab896584a46dd9862b40
Yaaza Ait Bah <aitbah.dev@gmail.com>
2026-03-05 09:30:46 +0000

    refactor(hooks): rendre le hook useAuthToast generique et plus robuste

--- src/hooks/useAuthToast.js
+++ src/hooks/useAuthToast.js
@@ -5,10 +5,11 @@ import { toast } from "sonner";
 /**
  * Hook pour afficher les notifications toast selon l'etat d'authentification
  */
-export function useAuthToast(status, code, t) {
+export function useAuthToast(status, code, t, type) {
   useEffect(() => {
     if (status === AUTH_STATUS.AUTHENTICATED)
-      toast.success(t("login.toast.success"));
-    else if (status === AUTH_STATUS.UNAUTHENTICATED) toast.error(t(code));
-  }, [status, code, t]);
+      toast.success(t(`${type}.toast.success`));
+    else if (code && status === AUTH_STATUS.UNAUTHENTICATED)
+      toast.error(t(code));
+  }, [status, code, t, type]);
 }
