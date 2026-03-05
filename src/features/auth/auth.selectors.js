09ddc69024bfcadfd6282252b40f6212ba48bb11
Yaaza Ait Bah <aitbah.dev@gmail.com>
2026-03-05 09:08:21 +0000

    refactor(auth): ameliorer les selecteurs en utilisant la composition

--- src/features/auth/auth.selectors.js
+++ src/features/auth/auth.selectors.js
@@ -1,8 +1,8 @@
-// Selector pour acceder à l'ensemble du slice 'auth' dans le store Redux
+// Selector pour acceder a l'ensemble du slice 'auth' dans le store Redux
 export const authSelector = (state) => state.auth;
 // Selector pour acceder au statut de l'authentification
-export const statusSelector = (state) => state.auth.status;
-// Selector pour acceder à la valeur 'checked' de l'auth
-export const checkedSelector = (state) => state.auth.checked;
+export const authStatusSelector = (state) => authSelector(state).status;
+// Selector pour acceder a la valeur 'checked' de l'auth
+export const authCheckedSelector = (state) => authSelector(state).checked;
 // Selector pour acceder aux erreurs d'authentification
-export const errorSelector = (state) => state.auth.error;
+export const authErrorSelector = (state) => authSelector(state).error;
