0cfa98aa8fd4ced915baa270f70a364f760ce0fa
Yaaza Ait Bah <aitbah.dev@gmail.com>
2026-03-09 07:48:13 +0000

    feat(auth): ajouter le schema de validation zod pour le passwordReset

--- src/features/auth/auth.schemas.js
+++ src/features/auth/auth.schemas.js
@@ -59,3 +59,53 @@ export const registerSchema = z
     message: "validation.confirmed",
     path: ["password_confirmation"],
   });
+
+/**
+ * Schema de validation pour le formulaire de demander le code de reinitialisation le mot de passe
+ * Utilisation de Zod pour valider les champs avec des cles de traduction
+ */
+export const sendResetCodeSchema = z.object({
+  email: z
+    .string("validation.string")
+    .trim()
+    .min(1, "validation.required")
+    .email("validation.email")
+    .max(255, "validation.max.string")
+    .regex(
+      /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+      "validation.regex",
+    ),
+});
+
+/**
+ * Schema de validation pour le formulaire de reinitialiser le mot de passe avec le code
+ * Utilisation de Zod pour valider les champs avec des cles de traduction
+ */
+export const resetPasswordSchema = z
+  .object({
+    email: z
+      .string("validation.string")
+      .trim()
+      .min(1, "validation.required")
+      .email("validation.email")
+      .max(255, "validation.max.string")
+      .regex(
+        /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+        "validation.regex",
+      ),
+    code: z
+      .string("validation.string")
+      .min(1, "validation.required")
+      .length(6, "validation.size.string"),
+    password: z
+      .string("validation.string")
+      .min(1, "validation.required")
+      .min(8, "validation.min.string"),
+    password_confirmation: z
+      .string("validation.string")
+      .min(1, "validation.required"),
+  })
+  .refine((data) => data.password === data.password_confirmation, {
+    message: "validation.confirmed",
+    path: ["password_confirmation"],
+  });
