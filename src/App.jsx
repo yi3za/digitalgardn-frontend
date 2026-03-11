9e30911a487a65b92fcee54d90e3fb4ab672092a
Yaaza Ait Bah <aitbah.dev@gmail.com>
2026-03-11 09:29:07 +0000

    feat(auth): recuperer l'utilisateur connecte au chargement de l'application

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,22 @@ import AppRouter from "@/routes/AppRouter";
 import { store } from "./store";
 import { Provider } from "react-redux";
 import { Toaster } from "sonner";
+import { useEffect } from "react";
+import { getMeThunk } from "./features/auth/auth.thunks";
 
 /**
  * Composant principal de l'application
  *
- * Il initialise le store
- * Affiche le routeur
+ * Initialise le store Redux
+ * Charge l'utilisateur connecte (auth initiale)
+ * Affiche le routeur et les notifications
  */
 export default function App() {
+  // Au montage du composant, recuperer l'utilisateur connecte
+  useEffect(() => {
+    store.dispatch(getMeThunk());
+  }, []);
+
   return (
     <Provider store={store}>
       <AppRouter />
