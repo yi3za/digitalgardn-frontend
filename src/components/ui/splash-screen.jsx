06fa7c1bbd2e094d86dcfecd11e155cef21f3d41
Yaaza Ait Bah <aitbah.dev@gmail.com>
2026-03-11 10:36:19 +0000

    feat(ui): ajouter un composant SplashScreen pour le chargement initial

---
+++ src/components/ui/splash-screen.jsx
@@ -0,0 +1,9 @@
+import { Spinner } from ".";
+
+export function SplashScreen() {
+  return (
+    <div className="fixed inset-0 z-9999 flex items-center justify-center">
+      <Spinner className="size-8" />
+    </div>
+  );
+}
