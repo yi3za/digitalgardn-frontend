865d11ab86faf4d20e4eaa6e7e67fc7cbb151f4a
Yaaza Ait Bah <aitbah.dev@gmail.com>
2026-03-12 07:42:05 +0000

    feat(router): afficher le SplashScreen pendant la verification initiale de l'authentification

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,5 +1,9 @@
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router";
+import { useSelector } from "react-redux";
+import { authStatusSelector } from "@/features/auth/auth.selectors";
+import { AUTH_STATUS } from "@/features/auth/auth.constants";
+import { SplashScreen } from "@/components/ui/splash-screen";
 
 /**
  * Composant responsable de la configuration du routage global
@@ -8,5 +12,10 @@ import { router } from "./router";
  * utilise RouterProvider pour rendre le routage accessible dans l'application
  */
 export function AppRouter() {
+  // Recuperation du statut d'authentification
+  const status = useSelector(authStatusSelector);
+  // Afficher SplashScreen tant que le statut d'authentification est IDLE
+  if (status === AUTH_STATUS.IDLE) return <SplashScreen />;
+
   return <RouterProvider router={router} />;
 }
