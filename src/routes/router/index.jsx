2cf8fdae110be75c29ec41c7eb9b315f2738fcf5
Yaaza Ait Bah <aitbah.dev@gmail.com>
2026-03-12 10:12:50 +0000

    refactor(router): appliquer le MainLayout comme parent de toutes les routes

--- src/routes/router/index.jsx
+++ src/routes/router/index.jsx
@@ -1,14 +1,20 @@
 import { createBrowserRouter } from "react-router-dom";
 import { authRoutes } from "./auth.routes";
 import { HomePage } from "@/pages/HomePage";
+import { MainLayout } from "@/components/layout/MainLayout";
 
 /**
  * Creation du routeur principal de l'application
  */
 export const router = createBrowserRouter([
   {
-    path: "/",
-    element: <HomePage />,
+    element: <MainLayout />,
+    children: [
+      {
+        path: "/",
+        element: <HomePage />,
+      },
+      authRoutes,
+    ],
   },
-  authRoutes,
 ]);
