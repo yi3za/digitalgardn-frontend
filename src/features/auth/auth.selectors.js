7fecc8b9b040ad4fdb49bfda4704524fbcc4e0ac
Yaaza Ait Bah <aitbah.dev@gmail.com>
2026-03-12 08:03:50 +0000

    feat(auth): ajouter un selecteur pour acceder aux donnees de l'utilisateur

--- src/features/auth/auth.selectors.js
+++ src/features/auth/auth.selectors.js
@@ -1,5 +1,7 @@
 // Selector pour acceder a l'ensemble du slice 'auth' dans le store Redux
 export const authSelector = (state) => state.auth;
+// Selector pour acceder a l'utilisateur connecte
+export const authUserSelector = (state) => authSelector(state).user;
 // Selector pour acceder au statut de l'authentification
 export const authStatusSelector = (state) => authSelector(state).status;
 // Selector pour savoir si une requete d'authentification est en cours
