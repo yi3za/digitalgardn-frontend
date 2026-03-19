5814bda0177b888f639296852dea90c4f915e7ff
Yaaza Ait Bah <aitbah.dev@gmail.com>
2026-03-19 22:59:02 +0000

    refactor(auth): simplifier les selectors du slice auth

--- src/features/auth/auth.selectors.js
+++ src/features/auth/auth.selectors.js
@@ -1,10 +1,2 @@
 // Selector pour acceder a l'ensemble du slice 'auth' dans le store Redux
 export const authSelector = (state) => state.auth;
-// Selector pour acceder a l'utilisateur connecte
-export const authUserSelector = (state) => authSelector(state).user;
-// Selector pour acceder au statut de l'authentification
-export const authStatusSelector = (state) => authSelector(state).status;
-// Selector pour savoir si une requete d'authentification est en cours
-export const authLoadingSelector = (state) => authSelector(state).loading;
-// Selector pour acceder aux erreurs d'authentification
-export const authErrorSelector = (state) => authSelector(state).error;
