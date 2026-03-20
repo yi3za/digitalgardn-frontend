5dbe5eef701e507de459fd97cf0f46e3f673d9c1
Yaaza Ait Bah <aitbah.dev@gmail.com>
2026-03-20 14:14:30 +0000

    fix(router): utiliser children au lieu de Outlet dans ProtectedRoute

--- src/routes/guards/ProtectedRoute.jsx
+++ src/routes/guards/ProtectedRoute.jsx
@@ -1,15 +1,15 @@
 import { AUTH_STATUS } from "@/features/auth/auth.constants";
 import { authSelector } from "@/features/auth/auth.selectors";
 import { useSelector } from "react-redux";
-import { Navigate, Outlet } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 
 /**
  * Route qui protege les pages reservees aux utilisateurs connectes
  */
-export function ProtectedRoute() {
+export function ProtectedRoute({ children }) {
   const { status } = useSelector(authSelector);
   // Si l'utilisateur n'est pas authentifie, redirection vers login
   if (status === AUTH_STATUS.UNAUTHENTICATED)
     return <Navigate to="/login" replace />;
-  return <Outlet />;
+  return children;
 }
