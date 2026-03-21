99e8375fe6f0ab584f001ec770685a74dcd6e06d
Yaaza Ait Bah <aitbah.dev@gmail.com>
2026-03-21 16:28:15 +0000

    fix(router): utiliser outlet au lieu de children dans ProtectedRoute et GuestRoute

--- src/routes/guards/GuestRoute.jsx
+++ src/routes/guards/GuestRoute.jsx
@@ -1,14 +1,14 @@
 import { AUTH_STATUS } from "@/features/auth/auth.constants";
 import { authSelector } from "@/features/auth/auth.selectors";
 import { useSelector } from "react-redux";
-import { Navigate } from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
 
 /**
  * Route qui protege les pages reservees aux visiteurs non connectes
  */
-export function GuestRoute({ children }) {
+export function GuestRoute() {
   const { status } = useSelector(authSelector);
   // Si l'utilisateur est deja authentifie, il est redirige vers la page d'accueil
   if (status === AUTH_STATUS.AUTHENTICATED) return <Navigate to="/" replace />;
-  return children;
+  return <Outlet />;
 }
--- src/routes/guards/ProtectedRoute.jsx
+++ src/routes/guards/ProtectedRoute.jsx
@@ -1,15 +1,15 @@
 import { AUTH_STATUS } from "@/features/auth/auth.constants";
 import { authSelector } from "@/features/auth/auth.selectors";
 import { useSelector } from "react-redux";
-import { Navigate } from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
 
 /**
  * Route qui protege les pages reservees aux utilisateurs connectes
  */
-export function ProtectedRoute({ children }) {
+export function ProtectedRoute() {
   const { status } = useSelector(authSelector);
   // Si l'utilisateur n'est pas authentifie, redirection vers login
   if (status === AUTH_STATUS.UNAUTHENTICATED)
     return <Navigate to="/login" replace />;
-  return children;
+  return <Outlet />;
 }
