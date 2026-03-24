8a148af900b083462fca69137f942933eaa5d293
Yaaza Ait Bah <aitbah.dev@gmail.com>
2026-03-24 11:51:13 +0100

    refactor(router): ajout SettingsItemsLayout et route personal-info dans parametres

--- src/routes/router/settings.routes.jsx
+++ src/routes/router/settings.routes.jsx
@@ -1,11 +1,22 @@
+import { SettingsItemsLayout } from "@/components/layout/SettingsItemsLayout";
+import { PersonalInfoPage } from "@/pages/settings/PersonalInfoPage";
 import { SettingsPage } from "@/pages/settings/settingsPage";
 
 /**
  * Definit les routes liees aux parametres du compte utilisateur
  *
+ * SettingsItemsLayout : layout pour les pages de gestion des parametres utilisateur
+ *
  * settings : page principale des parametres du compte utilisateur
+ * personal-info : page de gestion des informations personnelles de l'utilisateur
  */
 export const settingsRoutes = {
   path: "settings",
-  children: [{ index: true, element: <SettingsPage /> }],
+  children: [
+    { index: true, element: <SettingsPage /> },
+    {
+      element: <SettingsItemsLayout />,
+      children: [{ path: "personal-info", element: <PersonalInfoPage /> }],
+    },
+  ],
 };
