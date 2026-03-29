918e5b807d215c73eaace28495a7500af09082af
Yaaza Ait Bah <aitbah.dev@gmail.com>
2026-03-29 16:13:00 +0100

    refactor(ui): modifier le composant field

--- src/components/ui/field.jsx
+++ src/components/ui/field.jsx
@@ -48,7 +48,7 @@ function FieldGroup({
     <div
       data-slot="field-group"
       className={cn(
-        "group/field-group @container/field-group flex w-full flex-col gap-7 data-[slot=checkbox-group]:gap-3 [&>[data-slot=field-group]]:gap-4",
+        "group/field-group @container/field-group flex w-full flex-col gap-5 data-[slot=checkbox-group]:gap-3 [&>[data-slot=field-group]]:gap-4",
         className
       )}
       {...props} />
