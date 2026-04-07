4d017923e724d2f5c84fb759128427b4f00fff4a
Yaaza Ait Bah <aitbah.dev@gmail.com>
2026-04-07 18:52:01 +0100

    feat(onboarding): implementation de la page FreelancerSetupPage avec integration profil

--- src/pages/onboarding/FreelancerSetupPage.jsx
+++ src/pages/onboarding/FreelancerSetupPage.jsx
@@ -1,5 +1,33 @@
+import {
+  Button,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui";
+import { ProfilPage } from "../profil/ProfilPage";
+import { useTranslation } from "react-i18next";
+
+/**
+ * La page de configuration (setup) pour les freelances
+ */
 export function FreelancerSetupPage() {
-  return <div>
-    hello world
-  </div>;
+  // Hook de traduction
+  const { t } = useTranslation("onboarding");
+
+  return (
+    <>
+      <CardHeader>
+        <CardTitle>{t("title")}</CardTitle>
+        <CardDescription>{t("description")}</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <ProfilPage />
+      </CardContent>
+      <CardFooter className="justify-end">
+        <Button>{t("actions.submit")}</Button>
+      </CardFooter>
+    </>
+  );
 }
