fec1b1789ad25ac542cab2c418af47e5bce2a332
Yaaza Ait Bah <aitbah.dev@gmail.com>
2026-04-15 11:55:20 +0100

    feat(services): introduction d'un systeme d'etapes (stepper) pour la creation de service

--- src/pages/dashboard/services/ServiceCreatePage.jsx
+++ src/pages/dashboard/services/ServiceCreatePage.jsx
@@ -28,6 +28,7 @@ import { useForm } from "react-hook-form";
 import { useTranslation } from "react-i18next";
 import { useNavigate } from "react-router-dom";
 import { toast } from "sonner";
+import { useState } from "react";
 
 /**
  * Page de creation d'un service
@@ -50,6 +51,12 @@ export function ServiceCreatePage() {
     },
     resolver: zodResolver(storeServiceSchema),
   });
+  // Gestion des etapes du formulaire d'inscription
+  const [step, setStep] = useState(1);
+  // Passe a l'etape suivante
+  const next = () => setStep((s) => s + 1);
+  // Revenire a l'etape precedente
+  const back = () => setStep((s) => s - 1);
   /**
    * Fonction de soumission du formulaire
    */
@@ -57,7 +64,7 @@ export function ServiceCreatePage() {
     try {
       const { code } = await createServiceMutation.mutateAsync(data);
       toast.success(t(`codes:${code}`));
-      navigate("/dashboard/services");
+      next();
     } catch ({ code, details: errors }) {
       // Afficher les erreurs du serveur dans le formulaire
       setServerErrors(errors, form.setError);
@@ -86,76 +93,86 @@ export function ServiceCreatePage() {
         <Form {...form}>
           <FieldSet disabled={createServiceMutation.isPending}>
             <FieldGroup>
-              <CustomFormField
-                autoFocus
-                name="titre"
-                label={t("services.create.fields.titre.label")}
-                placeholder={t("services.create.fields.titre.placeholder")}
-                control={form.control}
-                icon={FileText}
-                rules={{ max: 255 }}
-              />
-              <FormField
-                name="description"
-                control={form.control}
-                render={({ field }) => {
-                  const label = t("services.create.fields.description.label");
-                  const length = field.value?.length || 0;
-                  const maxLength = 600;
-                  return (
-                    <FormItem className="relative">
-                      <FormLabel>{label}</FormLabel>
-                      <FormControl>
-                        <Textarea
-                          placeholder={t(
-                            "services.create.fields.description.placeholder",
-                          )}
-                          className="min-h-30"
-                          {...field}
-                        />
-                      </FormControl>
-                      <div className="absolute bottom-2 right-2">
-                        {length}/{maxLength}
-                      </div>
-                      <FormMessage
-                        rules={{ attribute: label, min: 150, max: 600 }}
-                      />
-                    </FormItem>
-                  );
-                }}
-              />
-              <CustomFormField
-                name="prix_base"
-                type="number"
-                label={t("services.create.fields.prix_base.label")}
-                placeholder={t("services.create.fields.prix_base.placeholder")}
-                control={form.control}
-                icon={DollarSign}
-                rules={{ min: 0 }}
-                min={0}
-              />
-              <CustomFormField
-                name="delai_livraison"
-                type="number"
-                label={t("services.create.fields.delai_livraison.label")}
-                placeholder={t(
-                  "services.create.fields.delai_livraison.placeholder",
-                )}
-                control={form.control}
-                icon={Clock}
-                rules={{ min: 1 }}
-                min={1}
-              />
-              <CustomFormField
-                name="revisions"
-                type="number"
-                label={t("services.create.fields.revisions.label")}
-                placeholder={t("services.create.fields.revisions.placeholder")}
-                control={form.control}
-                icon={RotateCcw}
-                rules={{ min: 0 }}
-                min={0}
-              />
+              {step === 1 && (
+                <>
+                  <CustomFormField
+                    autoFocus
+                    name="titre"
+                    label={t("services.create.fields.titre.label")}
+                    placeholder={t("services.create.fields.titre.placeholder")}
+                    control={form.control}
+                    icon={FileText}
+                    rules={{ max: 255 }}
+                  />
+                  <FormField
+                    name="description"
+                    control={form.control}
+                    render={({ field }) => {
+                      const label = t(
+                        "services.create.fields.description.label",
+                      );
+                      const length = field.value?.length || 0;
+                      const maxLength = 600;
+                      return (
+                        <FormItem className="relative">
+                          <FormLabel>{label}</FormLabel>
+                          <FormControl>
+                            <Textarea
+                              placeholder={t(
+                                "services.create.fields.description.placeholder",
+                              )}
+                              className="min-h-30"
+                              {...field}
+                            />
+                          </FormControl>
+                          <div className="absolute bottom-2 right-2">
+                            {length}/{maxLength}
+                          </div>
+                          <FormMessage
+                            rules={{ attribute: label, min: 150, max: 600 }}
+                          />
+                        </FormItem>
+                      );
+                    }}
+                  />
+                  <CustomFormField
+                    name="prix_base"
+                    type="number"
+                    label={t("services.create.fields.prix_base.label")}
+                    placeholder={t(
+                      "services.create.fields.prix_base.placeholder",
+                    )}
+                    control={form.control}
+                    icon={DollarSign}
+                    rules={{ min: 0 }}
+                    min={0}
+                  />
+                  <CustomFormField
+                    name="delai_livraison"
+                    type="number"
+                    label={t("services.create.fields.delai_livraison.label")}
+                    placeholder={t(
+                      "services.create.fields.delai_livraison.placeholder",
+                    )}
+                    control={form.control}
+                    icon={Clock}
+                    rules={{ min: 1 }}
+                    min={1}
+                  />
+                  <CustomFormField
+                    name="revisions"
+                    type="number"
+                    label={t("services.create.fields.revisions.label")}
+                    placeholder={t(
+                      "services.create.fields.revisions.placeholder",
+                    )}
+                    control={form.control}
+                    icon={RotateCcw}
+                    rules={{ min: 0 }}
+                    min={0}
+                  />
+                </>
+              )}
             </FieldGroup>
           </FieldSet>
         </Form>
