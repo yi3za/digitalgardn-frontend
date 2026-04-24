9a0f2076f87ba2653c0f2263de4cae4bdb5b868d
Yaaza Ait Bah <aitbah.dev@gmail.com>
2026-04-24 10:59:37 +0100

    style(ui): ajoute break-all pour eviter depassement sur contenu long

--- src/components/ui/card.jsx
+++ src/components/ui/card.jsx
@@ -10,7 +10,7 @@ function Card({
     <div
       data-slot="card"
       className={cn(
-        "flex flex-col gap-6 rounded-xl border bg-card py-6 text-card-foreground shadow-sm",
+        "flex flex-col gap-6 rounded-xl border bg-card py-6 text-card-foreground shadow-sm break-all",
         className
       )}
       {...props} />
