89057eb6ef30d726e3ad4bb1fcf6337846a7700c
Yaaza Ait Bah <aitbah.dev@gmail.com>
2026-04-27 16:58:52 +0100

    feat(realtime): abonnement global via hook useRealtimeSubscriptions

--- src/components/layout/MainLayout.jsx
+++ src/components/layout/MainLayout.jsx
@@ -1,6 +1,9 @@
 import { Outlet } from "react-router-dom";
 import { Header } from "./header";
 import { Footer } from "./footer";
+import { useSelector } from "react-redux";
+import { authSelector } from "@/features/auth/auth.selectors";
+import { useRealtimeSubscriptions } from "@/hooks/useRealtimeSubscriptions";
 
 /**
  * Composant MainLayout
@@ -8,6 +11,11 @@ import { Footer } from "./footer";
  * Layout principal de l'application
  */
 export function MainLayout() {
+  // Recuperation de l'utilisateur connecte
+  const { user } = useSelector(authSelector);
+  // Abonnements en temps reel
+  useRealtimeSubscriptions(user?.id);
+
   return (
     <div className="container mx-auto flex flex-col min-h-screen">
       {/* Header */}
