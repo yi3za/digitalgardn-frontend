21b2a648e1007027509627408e6b3cd10b380664
Yaaza Ait Bah <aitbah.dev@gmail.com>
2026-05-02 21:31:58 +0100

    style(shared): corriger l affichage des images dans le carousel

--- src/components/shared/ServiceDetailsCard.jsx
+++ src/components/shared/ServiceDetailsCard.jsx
@@ -86,14 +86,14 @@ export function ServiceDetailsCard({
         <div className="grid grid-cols-1 lg:grid-cols-[1.2fr_1fr]">
           <div className="px-6 flex items-center justify-center">
             {serviceImages.length > 0 ? (
-              <Carousel className="rounded-lg border overflow-hidden w-full h-full bg-muted flex justify-center items-center">
+              <Carousel className="rounded-lg border overflow-hidden w-full h-full bg-muted">
                 <CarouselContent>
                   {serviceImages.map((imageUrl, index) => (
                     <CarouselItem key={`${service.id}-image-${index}`}>
                       <img
                         src={imageUrl}
                         alt={`${service?.titre}-${index + 1}`}
-                        className="object-contain"
+                        className="h-full w-full"
                       />
                     </CarouselItem>
                   ))}
