8576e676d38572d33185677d0b9098a0e9a4fc9a
Yaaza Ait Bah <aitbah.dev@gmail.com>
2026-05-10 00:05:40 +0100

    docs(feedback): ajouter documentation du splash screen

--- src/components/feedback/splash-screen.jsx
+++ src/components/feedback/splash-screen.jsx
@@ -1,5 +1,8 @@
 import { Spinner } from "../ui";
 
+/**
+ * Component de splash screen pour indiquer le chargement de l'application
+ */
 export function SplashScreen() {
   return (
     <div className="fixed inset-0 z-9999 flex items-center justify-center">
